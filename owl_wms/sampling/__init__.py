f423a370326a6530d5f5abcc0d080fdeb196df06
shahbuland <smatiana1@gmail.com>
2025-07-17 23:49:10 +0000

    add more documentation for models/trainers/samplers, remove old redundant code

--- owl_wms/sampling/__init__.py
+++ owl_wms/sampling/__init__.py
@@ -3,24 +3,33 @@ from .cfg import CFGSampler
 from .window import WindowCFGSampler
 
 def get_sampler_cls(sampler_id):
-    if sampler_id == "simple":
-        return SimpleSampler
-    elif sampler_id == "cfg":
-        return CFGSampler
-    elif sampler_id == "window":
-        return WindowCFGSampler
-    elif sampler_id == "av_window":
+    if sampler_id == "av_window":
+        """
+        Most basic Audio+Video sampler with CFG
+        """
         from .av_window import AVWindowSampler
         return AVWindowSampler
     elif sampler_id == "av_caching":
+        """
+        Audio+Video sampler with strong caching, doesn't renoise previous frames. Caches clean frames. Will not work without special algorithm.
+        """
         from .av_caching import AVCachingSampler
         return AVCachingSampler
     elif sampler_id == "av_causal":
+        """
+        Audio+Video sampler with causal sampling, caches noisy history on first diffusion step.
+        """
         from .av_window import CausalAVWindowSampler
         return CausalAVWindowSampler
     elif sampler_id == "av_causal_no_cfg":
+        """
+        Identical to av causal but skips cfg (ideal for distilled models)
+        """
         from .av_window import CausalAVWindowSamplerNoCFG
         return CausalAVWindowSamplerNoCFG
     elif sampler_id == "av_caching_one_step":
+        """
+        Identical to av_caching but with a hard assumption for one step to simplify
+        """
         from .av_caching import AVCachingOneStepSampler
         return AVCachingOneStepSampler
--- owl_wms/trainers/__init__.py
+++ owl_wms/trainers/__init__.py
@@ -1,25 +1,31 @@
 def get_trainer_cls(trainer_id):
-    if trainer_id == "rft":
-        from .gamerft_trainer import RFTTrainer
-        return RFTTrainer
     if trainer_id == "causvid":
+        """
+        CausvidTrainer does DMD to create few-step causal student
+        """
         from .causvid import CausVidTrainer
         return CausVidTrainer
     if trainer_id == "av":
+        """
+        Most basic trainer. Does audio + video training.
+        """
         from .av_trainer import AVRFTTrainer
         return AVRFTTrainer
     if trainer_id == "mixed_av":
+        """
+        Allows for datasets that are a mix of unlabelled (wrt controls) and labelled
+        """
         from .mixed_av_trainer import MixedAVRFTTrainer
         return MixedAVRFTTrainer
-    if trainer_id == "sforce1":
-        from .self_forcing_stage_1 import CausalDistillationTrainer
-        return CausalDistillationTrainer
-    if trainer_id == "sforce2":
-        from .self_forcing_stage_2 import SelfForceTrainer
-        return SelfForceTrainer
-    if trainer_id == "sforce3":
+    if trainer_id == "sforce":
+        """
+        Self force trainer does clean context + DMD to enable few step with KV caching (broken rn)
+        """
         from .sf_trainer import SelfForceTrainer
         return SelfForceTrainer
     if trainer_id == "ode_distill":
+        """
+        ODE regression matches student trajectories to teacher trajectories
+        """
         from .ode_regression import DistillODETrainer
         return DistillODETrainer
