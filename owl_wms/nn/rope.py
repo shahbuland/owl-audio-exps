9518c2e9adf94c1ffd2890946e5036d88dfa416a
Andrew Lapp <andrew@github.rew.la>
2025-07-23 03:13:10 -0400

    revert to efficient RoPE

--- owl_wms/nn/rope.py
+++ owl_wms/nn/rope.py
@@ -59,23 +59,28 @@ class AVRoPE(nn.Module):
         head_dim = config.d_model // config.n_heads
         assert p**2 + 1 == config.tokens_per_frame
 
-        base_freqs = RotaryEmbedding(
+        pos_emb = RotaryEmbedding(
             dim=head_dim // 4,  # Using half dimension since we only need 1D rotation
             freqs_for='pixel',
             max_freq=256
-        ).get_axial_freqs(config.n_frames, p + 1, p + 1)
+        )
+        # Rot features: (L, P+1, P+1, <pad>)
+        freqs = pos_emb.get_axial_freqs(
+            config.n_frames, p + 1, p + 1, 1, offsets=(0, 0, 0, 1)
+        ).view(config.n_frames, p + 1, p + 1, -1)
 
-        vid_freqs = base_freqs[:, :p, :p].reshape(config.n_frames, p**2, -1)  # top left square
-        aud_freqs = base_freqs[:, -1, -1].unsqueeze(1)  # bottom right item
+        vid_freqs = freqs[:, :p, :p].reshape(config.n_frames, p**2, -1)  # top left square
+        aud_freqs = freqs[:, -1, -1].unsqueeze(1)  # bottom right item
+
         freqs = torch.cat([vid_freqs, aud_freqs], dim=1).flatten(0, 1)
 
         cos, sin = freqs.cos()[..., ::2], freqs.sin()[..., ::2]  # subsampling
         self.cos = nn.Buffer(cos.contiguous(), persistent=False)
         self.sin = nn.Buffer(sin.contiguous(), persistent=False)
 
-    def forward(self, x_vidio, x_audio, offset: int = 0):
-        L_vidio, L_audio = x_vidio.size(2), x_audio.size(2)  # [B, H, T, Dh], T is (P^2, 1)
-        x = torch.cat((x_vidio, x_audio), dim=2)
+    def forward(self, x0, x1, offset: int = 0):
+        size0, size1 = x0.size(2), x1.size(2)  # [B, H, T, Dh], T is (P^2, 1)
+        x = torch.cat((x0, x1), dim=2)
         cos = self.cos[..., offset:offset + x.size(2), :]
         sin = self.sin[..., offset:offset + x.size(2), :]
         r0, r1 = x.float().unfold(-1, 2, 2).unbind(-1)
