d18b12aa4a9b1ee9104501e7d9a5c62cc9152877
Andrew Lapp <andrew@github.rew.la>
2025-07-25 17:24:13 -0400

    add timeout to nccl arg

--- owl_wms/utils/ddp.py
+++ owl_wms/utils/ddp.py
@@ -1,26 +1,28 @@
-import torch 
 import torch.distributed as dist
 import os
+import datetime as dt
 
-def setup(force=False):
+def setup(force=False, timeout=None):
+    init_kwargs = dict(timeout=dt.timedelta(seconds=timeout)) if timeout else {}
+
     if not force:
         try:
-            dist.init_process_group(backend="nccl")
+            dist.init_process_group(backend="nccl", **init_kwargs)
             global_rank = dist.get_rank()
             local_rank = int(os.environ.get("LOCAL_RANK", 0))
             world_size = dist.get_world_size()
-            
+
             return global_rank, local_rank, world_size
         except:
             return 0, 0, 1
     else:
-        dist.init_process_group(backend="nccl")
+        dist.init_process_group(backend="nccl", **init_kwargs)
         global_rank = dist.get_rank()
         local_rank = int(os.environ.get("LOCAL_RANK", 0))
         world_size = dist.get_world_size()
-        
+
         return global_rank, local_rank, world_size
-        
+
 def cleanup():
     if dist.is_available() and dist.is_initialized():
         dist.destroy_process_group()
