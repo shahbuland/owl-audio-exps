6c1ad0f16e2a0bc51c2d104ef5bd144d6c106e80
Andrew Lapp <andrew@github.rew.la>
2025-07-25 00:02:18 -0400

    try this

--- owl_wms/data/s3_cod_latent_audio.py
+++ owl_wms/data/s3_cod_latent_audio.py
@@ -101,9 +101,9 @@ def get_loader(batch_size, dataset_path, window_length):
         ds,
         batch_size=batch_size,
         collate_fn=collate_fn,
-        num_workers=1,
+        num_workers=4,
         drop_last=True,
         pin_memory=True,
-        prefetch_factor=4,
+        prefetch_factor=2,
         **loader_kwargs
     )
