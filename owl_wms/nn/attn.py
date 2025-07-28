de81f9fcdf3b81673851adf58edc1212a8a6982d
Andrew Lapp <andrew@github.rew.la>
2025-07-27 22:45:49 -0400

    caching in sampler experiment (try this fix)

--- owl_wms/nn/attn.py
+++ owl_wms/nn/attn.py
@@ -14,8 +14,8 @@ from .rope import FlatVideoRoPE
 torch.backends.cuda.enable_flash_sdp(enabled = True)
 from torch.nn.attention.flex_attention import flex_attention, create_block_mask, BlockMask
 
-create_block_mask = torch.compile(create_block_mask)
-flex_attention = torch.compile(flex_attention)
+create_block_mask = torch.compile(create_block_mask, dynamic=True)
+flex_attention = torch.compile(flex_attention, dynamic=True)
 
 
 def checkpoint(function, *args, **kwargs):
