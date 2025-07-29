c62a56c3f707d49c9eb47d11e70d1872b4220725
Andrew Lapp <andrew@github.rew.la>
2025-07-29 09:56:17 -0400

    revert: don't skip step 0

--- owl_wms/trainers/av_trainer.py
+++ owl_wms/trainers/av_trainer.py
@@ -191,7 +191,7 @@ class AVRFTTrainer(BaseTrainer):
                         timer.reset()
 
                         # Sampling commented out for now
-                        if self.total_step_counter != 0 and self.total_step_counter % self.train_cfg.sample_interval == 0:
+                        if self.total_step_counter % self.train_cfg.sample_interval == 0:
                             with ctx:
                                 eval_wandb_dict = self.eval_step(
                                     sample_loader, sampler, decode_fn, audio_decode_fn
